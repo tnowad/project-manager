c1e15a677a890ded04d5b57294151de3303d042a
Nguyen Minh Tuan <tnowad@gmail.com>
2023-05-14 00:17:45 +0700

    feat(api): add random quote

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import quotes from './quotes.json';
+const app = express();
+
+interface Quote {
+  author: string;
+  quote: string;
+}
+
+const data: Quote[] = quotes;
+
+app.get('/api/quote', (req, res) => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  res.json(randomQuote);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port: 3000');
+});
