da9deaa6283d3970f5cf5ee13d6b32881d6ce9d4
Nguyen Minh Tuan <tnowad@gmail.com>
2023-06-15 14:16:52 +0700

    feat(file): add method to save quote to file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import quotes from './quotes.json';
+import path from 'path';
+import fs from 'fs';
 const app = express();
 
 interface Quote {
@@ -20,8 +22,22 @@ app.post('/api/quotes', (req: Request, res: Response) => {
   const { author, quote } = req.body;
   const newQuote: Quote = { author, quote };
   quotes.push(newQuote);
+  saveQuotesToJSON();
   res.status(201).json(newQuote);
 });
+
+function saveQuotesToJSON() {
+  const quotesPath = path.join(__dirname, 'quotes.json');
+  const quotesJSON = JSON.stringify(quotes, null, 2);
+
+  fs.writeFile(quotesPath, quotesJSON, (err) => {
+    if (err) {
+      console.error('Error writing quotes to file:', err);
+    } else {
+      console.log('Quotes saved to file.');
+    }
+  });
+}
 
 app.listen(3000, () => {
   console.log('Server is running on port: 3000');
